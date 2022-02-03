b75dc0e4142a8f95c6533379e77454e23b91d473
Weslley Ferreira <weslley2404@gmail.com>
2022-02-03 01:06:12 -0300

    Alterando AMI

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "amis" {
   type = map
   default = {
-      "us-east-1" = "ami-04505e74c0741db8d"
+      "us-east-1" = "ami-04505e74c0741db8d" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type Free Tier elegible
   }
 }
